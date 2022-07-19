15da637baf5ff64fd694fd2ca312b5745adc8f84
Richard <xqtwkk@gmail.com>
2022-07-19 13:37:56 +0300

    fix for tests 1

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,10 +2,17 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 4000;
+        int variable = 0;
+        variable = 2000;
+        int value = 1000;
+        variable = 4000;
         int value2 = 3000;
         int variables = 5000;
         variables = 7000;
-        int value = 90000;
+        value = 90000;
+        int integer = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        myInteger = 1234567;
     }
 }
